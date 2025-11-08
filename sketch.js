492f63c41c06b4ce2289ddbd8abe2b7d658e7d45
ziem9243 <cconyinanya@gmail.com>
2025-11-07 20:37:45 -0500

    testing

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ let money;
 let health;
 let looks;
 let intelligence;
+let happiness;
 
 function setup() {
   createCanvas(400, 400);
@@ -17,6 +18,7 @@ function draw() {
   background(255);
   fill(220);
   rect(0,height-height/4,width,height/4);
+  rect(0,0,20, 20);
   fill(0);
   text("Age: " + age, 10, height-height/5);
   text("Money: " + money, 10, (height-height/5)+15);
